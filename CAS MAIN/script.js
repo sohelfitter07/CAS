4e94b97b7504d7f248d31ea53cac5b8288243601
sohelfitter07 <sohelfitter07@gmail.com>
2025-05-25 12:24:43 -0400

    24th May 2025

--- CAS MAIN/script.js
+++ CAS MAIN/script.js
@@ -1,3 +1,26 @@
+const hamburger = document.getElementById("hamburger");
+  const navList = document.getElementById("navbarList");
+
+  hamburger.addEventListener("click", () => {
+    navList.classList.toggle("active");
+  });
+
+  document.querySelectorAll('.dropdown > a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    if (window.innerWidth <= 768) {
+      e.preventDefault();
+      const dropdown = this.nextElementSibling;
+      if (dropdown.style.maxHeight && dropdown.style.maxHeight !== '0px') {
+        dropdown.style.maxHeight = '0px';
+        dropdown.style.padding = '0 0';
+      } else {
+        dropdown.style.maxHeight = '500px'; // or calculate scrollHeight
+        dropdown.style.padding = '10px 0';
+      }
+    }
+  });
+});
+
 const words = ['Repair', 'Installation', 'Maintenance', 'Project'];
     const wrapper = document.getElementById('wordWrapper');
     const leftText = document.getElementById('leftText');
